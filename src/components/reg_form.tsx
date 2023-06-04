47ac825524aad09d59a6061142e78e2e84720fb8
Joe <hey@KuyaJoe.dev>
2023-06-04 10:26:48 +0800

    add hoooks folders and rename files

---
+++ src/components/reg_form.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+export default function RegForm() {
+    return (
+        <div>
+            <h2>Registration</h2>
+            <form>
+                <input />
+                <input />
+                <input />
+                <input />
+
+            </form>
+        </div>
+    )
+}
